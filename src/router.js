3817897ef1d3f288c93825910cb2a640b9c13b68
Lola <lolahef@gmail.com>
2019-02-25 18:14:22 -0500

    Added routes for each page; / redirects to about for now

--- src/router.js
+++ src/router.js
@@ -1,15 +1,49 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Landing from './components/Landing'
+import About from './components/about/About.vue'
+import Portfolio from './components/portfolio/Portfolio.vue'
+import Writing from './components/writing/Writing.vue'
+import Contact from './components/contact/Contact.vue'
+import Resume from './components/etc/Resume.vue'
+import NotFound from './components/etc/NotFound.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'landing',
-      component: Landing
-    }
-  ]
+      redirect: '/about'
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About,
+    },
+    {
+      path: '/portfolio',
+      name: 'portfolio',
+      component: Portfolio,
+    },
+    {
+      path: '/writing',
+      name: 'writing',
+      component: Writing,
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact,
+    },
+    {
+      path: '/resume',
+      name: 'resume',
+      component: Resume,
+    },
+    {
+      path: '*',
+      component: NotFound,
+    },
+  ],
 })
