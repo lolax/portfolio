6f0aa3fb12a6ca37a57a31573de6b2095650244b
Lola <lolahef@gmail.com>
2019-02-25 18:15:19 -0500

    Added about page

---
+++ src/components/about/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class='about-container'>
+    <img class='about-img' src='../../assets/lola.jpg' />
+    <h2>It's great to meet you; I'm Lola.</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+}
+</script>
+
+<style scoped>
+.about-img {
+  width: 100%;
+  height: auto;
+}
+</style>
