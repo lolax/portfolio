d25cf4dadfda8cb2044860bfc9f09349458713da
Lola <lolahef@gmail.com>
2019-02-27 00:17:43 -0500

    Import & use VueProgressiveImage

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
+import VueProgressiveImage from 'vue-progressive-image'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(VueProgressiveImage)
 
 new Vue({
   router,
