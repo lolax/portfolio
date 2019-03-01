52f4d3098e99c71c3ede7d3cce93e5da7b84cd70
Lola <lolahef@gmail.com>
2019-03-01 17:27:45 -0500

    Added alt

--- src/components/connect/Connect.vue
+++ src/components/connect/Connect.vue
@@ -2,6 +2,7 @@
   <div class='connect-container'>
     <progressive-img
       class='side-img'
+      alt='Lola standing beside a statue in Geneva'
       :src="require('../../assets/genevastatue.jpg')"
       :placeholder="require('../../assets/genevastatue-small.jpg')"
     />
