b7e1dc875d77ee940b43c78660474c3ed684c576
Lola <lolahef@gmail.com>
2019-03-01 17:14:57 -0500

    Added content to custom 404 page

--- src/components/etc/NotFound.vue
+++ src/components/etc/NotFound.vue
@@ -1,6 +1,11 @@
 <template>
   <div class='notfound-container'>
-    <h1>404</h1>
+    <h2>404</h2>
+    <progressive-img
+      class='shrug-img'
+      :src="require('../../assets/shrug.jpg')"
+    />
+    <h3>Sorry, I couldn't find that page.</h3>
   </div>
 </template>
 
@@ -10,4 +15,17 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+  .shrug-img {
+    width: 70%;
+    -webkit-box-shadow: 0 0 3px 2px lightgrey;
+    -moz-box-shadow: 0 0 3px 2px lightgrey;
+    box-shadow: 0 0 3px 2px lightgrey;
+  }
+  h2 {
+    font-size: 2.2rem;
+  }
+  h3 {
+    font-size: 2rem;
+  }
+</style>
