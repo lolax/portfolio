1c1adbf4e9b67e09343f98084b1ad5acc58c1172
Lola <lolahef@gmail.com>
2019-03-01 00:13:18 -0500

    Added project data

---
+++ src/components/portfolio/projectData.js
@@ -0,0 +1,25 @@
+const projectData = [
+  { 
+    id: 1,
+    name: 'Backpaca',
+    description: 'Backpaca is a great app, world class, the best. I love it because it\'s about alpacas.',
+    links: [{text: 'Application', href: 'https://backpaca.app/', id: 'backpaca-app'}, {text: 'Repository', href: 'https://github.com/Lambda-School-Labs/labs9-map-scratcher/', id: 'backpaca-repo'}],
+    gif: 'backpaca.png'
+  },
+  { 
+    id: 2,
+    name: 'What\'s Cooking',
+    description: 'What\'s Cooking is a great app, world class, the best. I love it because it\'s about food.',
+    links: [{text: 'Application', href: 'https://whatscookin.netlify.com/', id: 'cookin-app'}, {text: 'Repository', href: 'https://github.com/lolax/L-L-Recipes', id: 'cookin-repo'}],
+    gif: 'recipes.png'
+  },
+  { 
+    id: 3,
+    name: 'Notes',
+    description: 'Notes is a great app, world class, the best. I love it because it\'s about notes.',
+    links: [{text: 'Application', href: 'https://lola-notes.surge.sh/', id: 'notes-app'}, {text: 'Backend Repository', href: 'https://github.com/lolax/back-end-project-week', id: 'notes-backend'}, {text: 'Frontend Repository', href: 'https://github.com/lolax/notes-frontend', id: 'notes-frontend'}],
+    gif: 'notes.png'
+  },
+]
+
+export default projectData
