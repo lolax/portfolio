7c0c6a5f4daec3bb3fc060f63b171636aa5c74e0
Lola <lolahef@gmail.com>
2019-03-08 15:41:50 -0500

    Routing

--- src/router.js
+++ src/router.js
@@ -3,11 +3,10 @@ import Router from 'vue-router'
 import About from './components/about/About.vue'
 import Portfolio from './components/portfolio/Portfolio.vue'
 import Writing from './components/writing/Writing.vue'
-import Post from './components/writing/posts/Post.vue'
 import Connect from './components/connect/Connect.vue'
 import Resume from './components/etc/Resume.vue'
 import NotFound from './components/etc/NotFound.vue'
-
+ 
 Vue.use(Router)
 
 export default new Router({
@@ -32,11 +31,11 @@ export default new Router({
       name: 'writing',
       component: Writing,
     },
-    {
-      path: '/writing/:title',
-      name: 'post',
-      component: Post,
-    },
+    // {
+    //   path: '/posts',
+    //   name: 'post',
+    //   component: Post,
+    // },
     {
       path: '/connect',
       name: 'connect',
