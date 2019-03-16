f72107e606a4630b283970129a92abeb5917a485
Lola <lolahef@gmail.com>
2019-03-16 18:41:45 -0400

    Updated links in project data

--- src/components/portfolio/projectData.js
+++ src/components/portfolio/projectData.js
@@ -8,16 +8,16 @@ const projectData = [
   },
   { 
     id: 2,
-    name: 'What\'s Cooking',
+    name: 'Recipes',
     description: 'What\'s Cooking is a great app, world class, the best. I love it because it\'s about food.',
-    links: [{text: 'Application', href: 'https://whatscookin.netlify.com/', id: 'cookin-app'}, {text: 'Repository', href: 'https://github.com/lolax/L-L-Recipes', id: 'cookin-repo'}],
+    links: [{text: 'Application', href: 'https://recipes.lola.codes/', id: 'cookin-app'}, {text: 'Repository', href: 'https://github.com/lolax/L-L-Recipes', id: 'cookin-repo'}],
     gif: 'recipes.png'
   },
   { 
     id: 3,
     name: 'Notes',
     description: 'Notes is a great app, world class, the best. I love it because it\'s about notes.',
-    links: [{text: 'Application', href: 'https://lola-notes.surge.sh/', id: 'notes-app'}, {text: 'Backend Repository', href: 'https://github.com/lolax/back-end-project-week', id: 'notes-backend'}, {text: 'Frontend Repository', href: 'https://github.com/lolax/notes-frontend', id: 'notes-frontend'}],
+    links: [{text: 'Application', href: 'https://notes.lola.codes/', id: 'notes-app'}, {text: 'Backend Repository', href: 'https://github.com/lolax/back-end-project-week', id: 'notes-backend'}, {text: 'Frontend Repository', href: 'https://github.com/lolax/notes-frontend', id: 'notes-frontend'}],
     gif: 'notes.png'
   },
 ]
